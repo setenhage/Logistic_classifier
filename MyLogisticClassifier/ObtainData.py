73b58bc15d067615c26c400f32ec2e8f2019e71b
setenhage <suzanne.tenhage@gmail.com>
2021-07-31 14:33:33 +0200

    Initial setup for ObtainData module

---
+++ MyLogisticClassifier/ObtainData.py
@@ -0,0 +1,26 @@
+# ObtainData
+"""
+Created on Sat Jul 31 14:25:42 2021
+
+@author: Suus ten Hage
+"""
+
+class ObtainData:
+    
+    def __init__(self, directory, filename):
+        self.dir = directory
+        self.filename = filename
+        dir = directory
+        
+    def load_from_dir_csv(self):
+        #load csv with filename from directory
+    
+    def create_mock_data(self):
+        #Create data
+        
+        #Store data
+    
+    def split_train_test(self):
+        #split train and test data with provided split values. 
+    
+    pass
