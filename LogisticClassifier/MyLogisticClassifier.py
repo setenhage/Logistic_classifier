720ae7e319e32bd2849f61cf9b3dcea8461ac519
setenhage <suzanne.tenhage@gmail.com>
2021-08-05 12:01:56 +0200

    working implementation of gradient descent

--- LogisticClassifier/MyLogisticClassifier.py
+++ LogisticClassifier/MyLogisticClassifier.py
@@ -7,19 +7,18 @@ Created on Tue Aug  3 12:06:44 2021
 
 import numpy as np
 
-class MyLogistiClassifier:
+class MyLogisticClassifier:
     '''Class to build a logistic regression binary classifier from scratch.
     
     Takes in features (X) and results (y) and returns trained model 
     parameters as well as accuracy on the test set.'''
     
-    def __init__(self, it = 10000, alpha = 0.01, normalize = True):
-        self.iterations = it #number of iterations for gradient descent
+    def __init__(self, iterations = 10000, alpha = 0.01, normalize = True):
+        self.iterations = iterations #number of iterations for gradient descent
         self.alpha = alpha #learning rate
         self.normalize = normalize #True if data needs Z-score normalization
-        self.theta = np.zeros((self.n,1)) #weight initialization
     
-    def normalize(self, X):
+    def Z_normalization(self, X):
         #Z-score normalization
         for i in range(self.n):
             X = (X - X.mean(axis = 0))/X.std(axis = 0)  
@@ -39,27 +38,32 @@ class MyLogistiClassifier:
     def fit(self, X, y):
         self.m = X.shape[0] #number of training examples
         self.n = X.shape [1] #number of features
+        self.theta = np.zeros((self.n,1)) #weight initialization
         
         #if data is not normalized yet, perform Z-score normalization
         if self.normalize == True:
-             X = self.normalize(X)
-            
-        self.losses = [] #initialize losses list 
+             X = self.Z_normalization(X)
+             
+        #initialize losses list    
+        losses = [None] * self.iterations 
+        
+        #Gradient descent
         for it in range(self.iterations):
-            #calculate z and h with current weights 
+            #calculate z and h with current weights
             z = np.dot(X, self.theta)
             h = self.sigmoid(z)
             
             #update weights
-            self.theta -= self.alpha * self.gradient(X, h, y)
+            self.theta -= self.alpha * self.gradients(X, h, y)
             
             #calculate z and h with new weights 
             z = np.dot(X, self.theta)
             h = self.sigmoid(z)   
             
             #calculate and store loss
-            losses[i] = self.loss(h, y)
-            
+            losses[it] = self.logistic_loss(h, y)
+        
+        return self.theta, losses
             
     
     pass
