4f50a0b42a3c24fa1478af138a9f608a644a5cc8
setenhage <suzanne.tenhage@gmail.com>
2021-09-14 11:08:22 -0700

    Added a test on non-linearly seperable data

--- MyLogisticClassifierTest.py
+++ MyLogisticClassifierTest.py
@@ -8,7 +8,9 @@ Created on Sat Jul 31 14:19:44 2021
 from LogisticClassifier.MyLogisticClassifier import MyLogisticClassifier
 import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.datasets import make_classification
+from sklearn.datasets import make_classification, make_moons
+
+## Test on linearly seperable dataset
 
 #create mock data
 X, y = make_classification(n_features = 2, n_redundant = 0, 
@@ -45,7 +47,37 @@ pred = model.predict(X_test, theta)
 accuracy = (pred == y_test).mean()
 print(accuracy)
 
+## Test on non-linearly seperable dataset
 
+X, y = make_moons(n_samples=100, noise=0.24)
+y = y.reshape(y.shape[0],1) #make sure y is proper size (m, 1) and not (m,)
 
+#split data in test and train
+index = np.arange(100)
+np.random.shuffle(index)
+training, test = index[:80], index[80:]
+
+X_train = X[training,:]
+y_train = y[training]
+
+X_test = X[test,:]
+y_test = y[test]
+
+#apply logistic classifier to mock data
+model =  MyLogisticClassifier(iterations = 20000, alpha = 0.01, 
+                              normalize = False, fit_intercept = True)
+[theta, J] = model.fit(X_train,y_train)
+
+#plot the costs curve
+plt.plot(J)
+plt.ylabel('losses')
+plt.show()
+
+#make predictions
+pred = model.predict(X_test, theta)
+
+#calculate prediction accuracy 
+accuracy = (pred == y_test).mean()
+print(accuracy)
 
 
